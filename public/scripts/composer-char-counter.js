43e7c48185013b0103344f79d08aa2ce5f35913d
Gleb Shkut <gleb.shkut@gmail.com>
2022-02-01 14:53:30 -0800

    jQuery counter implemented

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(() => {
+  const $textArea = $('textarea#tweet-text');
+  $textArea.on('keyup', function() {
+    let value = 140 - $textArea.val().length;
+    const $counter = $('section.new-tweet form div output.counter');
+    $counter.val(value);
+    if (value < 0) {
+      $counter.css('color', '#ff0000');
+    } else {
+      $counter.css('color', '#535149');
+    }
+  })
+});
